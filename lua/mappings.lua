3bcf85a459de31c0cc4fbce5b53d12615589442d
Danny Oh <dannyoh0316@gmail.com>
2021-11-25 17:30:23 -0600

    map ctrl-backspace to delete previous word while in insert mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,10 @@ local api = vim.api
 -- jk for exiting insert mode
 api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
+-- ctrl-backspace delete previous word while in insert mode
+api.nvim_set_keymap('i', '<C-BS>', '<C-w>', { noremap = true, silent = true })
+api.nvim_set_keymap('i', '<C-h>', '<C-w>', { noremap = true, silent = true })
+
 -- set leader to space
 api.nvim_set_keymap('n', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
