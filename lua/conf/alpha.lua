c2a3382099cf8851cd879584f677e5dc0abf1349
Danny Oh <dannyoh0316@gmail.com>
2022-03-15 22:07:14 -0500

    add i3 shortcut to alpha startup dashboard

--- lua/conf/alpha.lua
+++ lua/conf/alpha.lua
@@ -21,11 +21,10 @@ startify.section.top_buttons.val = {}
 startify.section.mru_cwd.val = { { type = 'padding', val = 0 } }
 
 startify.section.bottom_buttons.val = {
-  startify.button( 'e', 'empty', ':ene <BAR> startinsert <CR>'),
-  startify.button('i', 'init.lua', ':e $HOME/.config/nvim/init.lua<CR>'),
-  startify.button('l', 'lua', ':e $HOME/.config/nvim/lua<CR>'),
-  startify.button('p', 'polybar', ':e $HOME/.config/polybar<CR>'),
   startify.button('a', 'alacritty', ':e $HOME/.config/alacritty/alacritty.yml<CR>'),
+  startify.button('i', 'i3', ':e $HOME/.config/i3/config<CR>'),
+  startify.button('n', 'nvim', ':e $HOME/.config/nvim<CR>'),
+  startify.button('p', 'polybar', ':e $HOME/.config/polybar<CR>'),
   startify.button('z', 'zshrc', ':e $HOME/.config/zsh/.zshrc<CR>'),
   startify.button( 'q', 'quit' , ':qa<CR>'),
 }
