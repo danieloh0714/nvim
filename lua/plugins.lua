58342ba29d69b4698ffca86a4651ea82de31b655
Daniel Oh <danieloh0714@gmail.com>
2023-11-21 21:07:18 -0600

    switch plugin manager from Packer to Lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,34 +1,31 @@
-return require('packer').startup(function()
-  -- plugin manager
-  use 'wbthomason/packer.nvim'
-
+return {
   -- colorscheme
-  use 'kvrohit/rasmus.nvim'
+  'kvrohit/rasmus.nvim',
 
   -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- treesitter
-  use 'windwp/nvim-autopairs' -- autopairs
-  use 'windwp/nvim-ts-autotag' -- autotags for html, jsx, tsx, etc.
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }, -- treesitter
+  'windwp/nvim-autopairs', -- autopairs
+  'windwp/nvim-ts-autotag', -- autotags for html, jsx, tsx, etc.
 
   -- LSP
-  use 'neovim/nvim-lspconfig' --- language server protocol
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'hrsh7th/nvim-cmp' -- autocompletion
-  use 'saadparwaiz1/cmp_luasnip' -- snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- snippets plugin
-  use 'jose-elias-alvarez/null-ls.nvim' -- formatting
+  'neovim/nvim-lspconfig', --- language server protocol
+  'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+  'hrsh7th/nvim-cmp', -- autocompletion
+  'saadparwaiz1/cmp_luasnip', -- snippets source for nvim-cmp
+  'L3MON4D3/LuaSnip', -- snippets plugin
+  'jose-elias-alvarez/null-ls.nvim', -- formatting
 
   -- navigation
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim' -- file finder
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } -- fuzzy finder
-  use 'nvim-tree/nvim-tree.lua' -- tree explorer on side
-  use 'akinsho/nvim-bufferline.lua' -- tab-like buffers on top
-  use 'goolord/alpha-nvim' -- starting page
+  'nvim-lua/plenary.nvim',
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, -- fuzzy finder
+  'nvim-telescope/telescope.nvim', -- file finder
+  'nvim-tree/nvim-tree.lua', -- tree explorer on side
+  'akinsho/nvim-bufferline.lua', -- tab-like buffers on top
+  'goolord/alpha-nvim', -- starting page
 
   -- other
-  use 'akinsho/toggleterm.nvim' -- toggle terminal
-  use 'kyazdani42/nvim-web-devicons' -- icons
-  use 'NvChad/nvim-colorizer.lua' -- colorizer
-  use 'nvim-lualine/lualine.nvim' -- bottom status line
-end)
+  'akinsho/toggleterm.nvim', -- toggle terminal
+  'kyazdani42/nvim-web-devicons', -- icons
+  'NvChad/nvim-colorizer.lua', -- colorizer
+  'nvim-lualine/lualine.nvim', -- bottom status line
+}
