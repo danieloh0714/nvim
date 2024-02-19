be00d9ec3e065e5304757a2b8121ee0acc3df9e9
Daniel Oh <danieloh0714@gmail.com>
2024-02-18 22:00:28 -0600

    fix colorscheme config

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,13 +1,10 @@
 require('rose-pine').setup({
-    variant = 'main',
-    dim_inactive_windows = false,
-    extend_background_behind_borders = true,
-
-    styles = {
-      bold = false,
-      italic = false,
-      transparency = false,
-    },
+  variant = 'main',
+  styles = {
+    bold = false,
+    italic = false,
+    transparency = true,
+  },
 })
 
-vim.cmd[[colorscheme rose-pine]]
+vim.cmd [[colorscheme rose-pine]]
