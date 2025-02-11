08d18f313d9cfad41bd95072d8059d22ce26ce54
Daniel Oh <danieloh0714@gmail.com>
2025-02-11 02:52:51 -0600

    update alpha bookmarks

--- lua/conf/alpha.lua
+++ lua/conf/alpha.lua
@@ -5,10 +5,11 @@ startify.section.top_buttons.val = {}
 startify.section.mru_cwd.val = { { type = 'padding', val = 0 } }
 
 startify.section.bottom_buttons.val = {
-    startify.button('a', 'alacritty', ':e $HOME/.config/alacritty<cr>'),
-    startify.button('h', 'hyprland', ':e $HOME/.config/hypr/hyprland.conf<cr>'),
-    startify.button('n', 'nvim', ':e $HOME/.config/nvim<cr>'),
-    startify.button('w', 'waybar', ':e $HOME/.config/waybar<cr>'),
+    startify.button('a', 'alacritty', ':e $XDG_CONFIG_HOME/alacritty<cr>'),
+    startify.button('h', 'hyprland', ':e $XDG_CONFIG_HOME/hypr/hyprland.conf<cr>'),
+    startify.button('n', 'nvim', ':e $XDG_CONFIG_HOME/nvim<cr>'),
+    startify.button('s', 'scripts', ':e $HOME/.local/bin<cr>'),
+    startify.button('w', 'waybar', ':e $XDG_CONFIG_HOME/waybar<cr>'),
     startify.button('q', 'quit', ':qa<cr>'),
 }
 
